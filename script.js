63313d4ffe9b5c9cd810d2f4a5e8eed5bea94435
Eduard Badea <eduardbadea@outlook.com>
2025-01-16 17:13:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const words = []
+const words = [];
 
 function saveWord() {
-    const getWord = document.getElementById('word').value
+    const getWord = document.getElementById('word').value;
     if (words.length) {
         if (words.includes(getWord)) {
-            alert('The word is already saved')
+            alert('The word is already saved');
         } else {
-            words.push(getWord)
+            words.push(getWord);
         }
     } else {
-        words.push(getWord)
+        words.push(getWord);
     }
-    document.getElementById('word').value = ''
+    document.getElementById('word').value = '';
 }
 
 function searchWord() {
-    const wordSearch = document.getElementById('word').value
-    let index = words.indexOf(wordSearch)
+    const wordSearch = document.getElementById('word').value;
+    let index = words.indexOf(wordSearch);
     if (index == -1) {
-        alert('The searched word was not found!')
+        alert('The searched word was not found!');
     } else {
-        alert('The searched word was found!')
+        alert('The searched word was found!');
     }
-    document.getElementById('word').value = ''
+    document.getElementById('word').value = '';
 }
 
 function allowOnlyLetters(event) {
-    const charCode = event.which || event.keyCode
-    const char = String.fromCharCode(charCode)
+    const charCode = event.which || event.keyCode;
+    const char = String.fromCharCode(charCode);
     if (!/[A-Za-z]/.test(char)) {
         event.preventDefault(); 
     }
